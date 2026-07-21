4a2f2d2342cb406b1972d16bfdb29874dc45bff6
Oscar Perez <cari.rsm@gmail.com>
2026-07-21 03:27:31 -0700

    add create truck endpoint

--- app/routes/trucks.py
+++ app/routes/trucks.py
@@ -9,3 +9,11 @@ router = APIRouter()
 @router.get('/trucks', response_model=list[TruckResponse])
 def get_trucks(db: Session = Depends(get_db)):
     return db.query(Truck).all()
+
+@router.post('/trucks', status_code=201, response_model=TruckResponse)
+def create_truck(truck_data: TruckCreate, db: Session = Depends(get_db)):
+    new_truck = Truck(truck_no=truck_data.truck_no, model=truck_data.model, available=truck_data.available)
+    db.add(new_truck)
+    db.commit()
+    db.refresh(new_truck)
+    return new_truck
